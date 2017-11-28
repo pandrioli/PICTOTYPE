52fd860426bf1ec6985cf26e02e0ce86c3aed4bd
FlorLista <florlistamail@gmail.com>
2017-11-28 15:33:10 -0300

    bugfix

--- public/js/background.js
+++ public/js/background.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', startBackgroundTimer);
-var time = 0;
+var timeBackground = 0;
 var cycle = 2000;
 var radius = 1000;
 var pi = 3.1416;
@@ -9,9 +9,9 @@ function startBackgroundTimer() {
 }
 
 function moveBackground() {
-  time++;
-  time = time % cycle;
-  var t = time*pi/cycle*2;
+  timeBackground++;
+  timeBackground = timeBackground % cycle;
+  var t = timeBackground*pi/cycle*2;
   var x = Math.round(Math.sin(t) * radius);
   var y = Math.round(Math.cos(t) * radius);
   document.body.style.backgroundPosition = x + "px " + y + "px";
